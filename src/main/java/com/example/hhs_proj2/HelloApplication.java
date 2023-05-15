d5c6daf3ef7d29548ee3df5e148241ef0eda894d
Lossttt <lfatmi.ayoub@gmail.com>
2023-05-15 12:46:31 +0200

    Added stylesheet

--- src/main/java/com/example/hhs_proj2/HelloApplication.java
+++ src/main/java/com/example/hhs_proj2/HelloApplication.java
@@ -10,16 +10,20 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signup.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login2.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+        // Voeg css stylesheet toe
+        scene.getStylesheets().add(getClass().getResource("/com/example/hhs_proj2/stylesheet.css").toExternalForm());
+        // Gebruiker kan de windowgrootte niet aanpassen
         stage.setResizable(false);
+
         stage.setTitle("Welcome to 42's AI assistant!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-
         launch();
     }
 }
